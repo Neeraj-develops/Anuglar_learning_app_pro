519a5a130bed795752f93d866354032e9084b929
Neeraj Sharma <nesharma@manh.com>
2024-06-26 17:52:17 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Angular_app_pro',
+  webDir: 'www'
+};
+
+export default config;
